ae0e1d15c089ff9e3d02427c8fe1322ba236dd66
belau0129 <a0907282773@gmail.com>
2024-07-02 11:50:33 +0800

    我的第一支JAVA程式

---
+++ home.java
@@ -0,0 +1,12 @@
+package test;
+
+public class home{
+    public static void main(String args[]){
+        float num = 2 ;
+        char name = 'N' ;
+        String words = "hello" ;
+        System.out.println("the number is "+num) ;
+        System.out.println("The meaning of name is "+name) ;
+        System.out.println("The String of words is "+words) ;
+    }
+}

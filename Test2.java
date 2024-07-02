e8e798a8536511adfdc33c5ba41e7543c3916514
belau0129 <a0907282773@gmail.com>
2024-07-02 13:32:36 +0800

    增加test.java
    so good

---
+++ Test2.java
@@ -0,0 +1,10 @@
+package test ;
+// Ch2_1, 簡單的Java程式
+public class test { //定義public 類別test
+    public static void main(String args[]){ // main() 函數，主程式開始
+        int num = 2 ;   //宣告整數num
+        int num1 = 3 ;  //宣告整數num1
+        System.out.println(num+" cats are running");    //印出字串與整數內容
+        System.out.println(num1+" dogs are running") ;  //印出字串與整數內容
+    }
+}

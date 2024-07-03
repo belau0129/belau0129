08ead908a0bb0a5a4ea28f3a93160f62608c7562
belau0129 <a0907282773@gmail.com>
2024-07-03 11:06:58 +0800

    Ch2_program

---
+++ Ch2_2.java
@@ -0,0 +1,10 @@
+public class Ch2_2 {
+    public static void main(String args[]){
+        int a ;     //宣告變數但不設值
+        int b=4, sum ;      //宣告b和sum兩個變數，但給b設值為4
+
+        a=5 ;
+        sum=a+b ;
+        System.out.println("sum="+sum) ;
+    }
+}
---
+++ Ch3_1.java
@@ -0,0 +1,13 @@
+public class Ch3_1 {
+    public static void main(String args[]){
+        int num1,num2 ;
+        num1 = 1 ; 
+        num2 = 2 ;
+        float num3 = 4 ;
+        String num4 = "五" ;
+        System.out.println(num1+" cats are running") ;
+        System.out.println(num2+" dogs are running") ;
+        System.out.println(num3+" birds are running") ;
+        System.out.println(num4+" tigers are running") ;
+    }
+}

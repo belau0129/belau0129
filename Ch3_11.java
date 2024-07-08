3904de8a05752658edf1dc1fb98b4921c820f34e
belau0129 <a0907282773@gmail.com>
2024-07-08 14:30:02 +0800

    0708 progress

---
+++ Ch3_11.java
@@ -0,0 +1,6 @@
+public class Ch3_11 {
+    public static void main(String args[]) {
+        boolean status = false; 
+        System.out.println("status="+status) ;
+    }
+}
---
+++ Ch3_9_1.java
@@ -0,0 +1,14 @@
+public class Ch3_9_1 {
+    public static void main(String args[]) {
+        char c1 = 'G' ;
+        char c2 = 71 ;
+        char c3 = '\u0047' ;
+        int uni = '好' ;
+        int unii = '你' ;
+
+        System.out.println("c1="+c1+", c2="+c2+", c3="+c3) ;
+        System.out.println("uni="+(char)uni) ;
+        System.out.println((int)'好') ;
+        System.out.println("unii="+(char)unii) ;
+    }
+}
